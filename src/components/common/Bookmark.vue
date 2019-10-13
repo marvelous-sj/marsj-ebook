8150295d6057a6168ef5bec0e2c93eb23c699bf4
Marvelous_SJ <446454384@163.com>
2019-10-13 17:06:20 +0800

    book mark step three

--- src/components/common/Bookmark.vue
+++ src/components/common/Bookmark.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="bookmark"></div>
+  <div class="bookmark" :style="style" ref="bookmark"></div>
 </template>
 
 <script>
+  import { px2rem } from '../../utils/utils'
+
   export default {
     props: {
       width: Number,
       height: Number,
       color: String
+    },
+    computed: {
+      style () {
+        if (this.color) {
+          return {
+            borderColor: `${this.color} ${this.color} transparent ${this.color}`
+          }
+        } else {
+          return {}
+        }
+      }
+    },
+    methods: {
+      refresh () {
+        if (this.width && this.height) {
+          this.$refs.bookmark.style.borderWidth = `${px2rem(this.height - 5)}rem ${px2rem(this.width / 2)}rem ${px2rem(5)}rem ${px2rem(this.width / 2)}rem`
+        }
+      }
+    },
+    mounted () {
+      this.refresh()
     }
   }
 </script>
--- src/components/ebook/EbookBookmark.vue
+++ src/components/ebook/EbookBookmark.vue
@@ -1,27 +1,122 @@
 <template>
-  <div class="ebook-bookmark">
+  <div class="ebook-bookmark" ref="bookmark">
     <div class="ebook-bookmark-text-wrapper">
       <div class="ebook-bookmark-down-wrapper">
-        <span class="icon-down"></span>
+        <span class="icon-down" ref="iconDown"></span>
       </div>
       <div class="ebook-bookmark-text">{{text}}</div>
     </div>
-    <div class="ebook-bookmark-icon-wrapper">
-      <bookmark></bookmark>
+    <div class="ebook-bookmark-icon-wrapper" :style="isFixed ? fixedStyle : {}">
+      <bookmark :color="color" :width="15" :height="35"></bookmark>
     </div>
   </div>
 </template>
 
 <script>
   import Bookmark from '../common/Bookmark'
+  import { realPx } from '../../utils/utils'
+  import { ebookMixin } from '../../utils/mixin'
+
+  const BLUE = '#346cbc'
+  const WHITE = '#eaeeff'
 
   export default {
+    mixins: [ebookMixin],
     components: {
       Bookmark
     },
     data () {
       return {
-        text: this.$t('book.pulldownAddMark')
+        text: '',
+        color: WHITE,
+        isFixed: false
+      }
+    },
+    computed: {
+      height () {
+        return realPx(35)
+      },
+      threshold () {
+        return realPx(55)
+      },
+      fixedStyle () {
+        return {
+          position: 'fixed',
+          top: 0,
+          right: `${(window.innerWidth - this.$refs.bookmark.clientWidth) / 2}px`
+        }
+      }
+    },
+    watch: {
+      offsetY (v) {
+        if (!this.bookAvailable && this.menuVisible) {
+          return
+        }
+        if (this.height <= v && this.threshold > v) {
+          this.beforeThreshold(v)
+        } else if (v >= this.threshold) {
+          this.afterThreshold(v)
+        } else if (v > 0 && v <= this.height) {
+          this.beforeHeight()
+        } else if (v === 0) {
+          this.restore()
+        }
+      }
+    },
+    methods: {
+      addBookMark () {
+      },
+      removeBookMark () {
+      },
+      restore () {
+        setTimeout(() => {
+          this.$refs.bookmark.style.top = `${-this.height}px`
+          this.$refs.iconDown.style.transform = ''
+        }, 200)
+        if (this.isFixed) {
+          this.setIsBookmark(true)
+          this.addBookMark()
+        } else {
+          this.setIsBookmark(false)
+          this.removeBookMark()
+        }
+      },
+      beforeHeight () {
+        if (this.isBookmark) {
+          this.text = this.$t('book.pulldownDeleteMark')
+          this.color = BLUE
+          this.isFixed = true
+        } else {
+          this.text = this.$t('book.pulldownAddMark')
+          this.color = WHITE
+          this.isFixed = false
+        }
+      },
+      beforeThreshold (v) {
+        // 第二阶段
+        this.$refs.bookmark.style.top = `${-v}px`
+        this.beforeHeight()
+        const iconDown = this.$refs.iconDown
+        if (iconDown.style.transform === 'rotate(180deg)') {
+          iconDown.style.transform = ''
+        }
+      },
+      afterThreshold (v) {
+        // 第三阶段
+        this.$refs.bookmark.style.top = `${-v}px`
+        if (this.isBookmark) {
+          this.text = this.$t('book.releaseDeleteMark')
+          this.color = WHITE
+          this.isFixed = false
+        } else {
+          this.text = this.$t('book.releaseAddMark')
+          this.color = BLUE
+          this.isFixed = true
+        }
+        const iconDown = this.$refs.iconDown
+        if (iconDown.style.transform === '') {
+          iconDown.style.transform = 'rotate(180deg)'
+        }
       }
     }
   }
@@ -41,7 +136,7 @@
     .ebook-bookmark-text-wrapper {
       position: absolute;
       right: px2rem(45);
-      bottom: px2rem(5);
+      bottom: 0;
       display: flex;
 
       .ebook-bookmark-down-wrapper {
@@ -58,7 +153,6 @@
     .ebook-bookmark-icon-wrapper {
       position: absolute;
       right: 0;
-      bottom: px2rem(5);
       margin-right: px2rem(15);
     }
   }
