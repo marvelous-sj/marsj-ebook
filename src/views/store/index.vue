3dfad445305e50c6c5cc7892e728793583b8ddea
Marvelous_SJ <446454384@163.com>
2019-10-21 22:02:13 +0800

    init bookCity page

---
+++ src/views/store/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="store">
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+  .store {
+    width: 100%;
+    height: 100%;
+    background: white;
+  }
+</style>
---
+++ src/views/store/store-home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="store-home">1111</div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+.store-home {
+  width: 100%;
+  height: 100%;
+}
+</style>
