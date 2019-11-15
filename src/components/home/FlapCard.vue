523759d91cdc10dbeeb73a4b155b272966eafc0a
Marsj <446454384@163.com>
2019-11-15 08:35:31 +0800

    update flapCard

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flap-card-visible" v-show="flapCardVisible">
     <div class="flap-card-bg">
-      <div class="flap-card" v-for="(item, index) in flapCardList" :key="index">
+      <div class="flap-card" v-for="(item, index) in flapCardList" :key="index" :style="{zIndex:item.zIndex}">
         <div class="flap-card-circle">
           <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCircleStyle(item,'left')"></div>
           <div class="flap-card-semi-circle flap-card-semi-circle-right" :style="semiCircleStyle(item,'right')"></div>
@@ -26,9 +26,9 @@
       }
     },
     methods: {
-      semiCircleStyle (item, position){
+      semiCircleStyle (item, position) {
         return {
-          backgroundColor: `rgb(${item.r},${item.g}，${item.b})`,
+          backgroundColor: `rgb(${item.r},${item.g},${item.b})`,
           backgroundSize: item.backgroundSize,
           backgroundImage: position === 'left' ? item.imgLeft : item.imgRight
         }
@@ -44,16 +44,12 @@
   @import "../../assets/style/global";
 
   .flap-card-visible {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
     width: 100%;
     height: 100%;
     z-index: 1000;
     background: rgba(0, 0, 0, 0.6);
     @include center;
+    @include absCenter;
     .flap-card-bg{
       position: relative;
       width: px2rem(64);
@@ -61,10 +57,28 @@
       border-radius: px2rem(5);
       background: white;
       .flap-card{
+        width: px2rem(48);
+        height: px2rem(48);
+       @include absCenter;
         .flap-card-circle{
-          .flap-card-semi-circle{}
-          .flap-card-semi-circle-left{}
-          .flap-card-semi-circle-right{}
+          display: flex;
+          height: 100%;
+          width: 100%;
+          .flap-card-semi-circle{
+            flex: 0 0 50%;
+            height: 100%;
+            width: 50%;
+            background-repeat: no-repeat;
+          }
+          .flap-card-semi-circle-left{
+            border-radius: px2rem(24) 0 0 px2rem(24);
+            background-position: center right;
+
+          }
+          .flap-card-semi-circle-right{
+            border-radius: 0 px2rem(24) px2rem(24) 0;
+            background-position: center left;
+          }
         }
       }
     }
