7b05976398d4d0684131286a40ff81926584cc19
Marsj <446454384@163.com>
2019-11-19 22:36:26 +0800

    complete flap rotate animation

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -3,8 +3,16 @@
     <div class="flap-card-bg">
       <div class="flap-card" v-for="(item, index) in flapCardList" :key="index" :style="{zIndex:item.zIndex}">
         <div class="flap-card-circle">
-          <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCircleStyle(item,'left')"></div>
-          <div class="flap-card-semi-circle flap-card-semi-circle-right" :style="semiCircleStyle(item,'right')"></div>
+          <div
+            class="flap-card-semi-circle flap-card-semi-circle-left"
+            :style="semiCircleStyle(item,'left')"
+            ref="left"
+          ></div>
+          <div
+            class="flap-card-semi-circle flap-card-semi-circle-right"
+            :style="semiCircleStyle(item,'right')"
+            ref="right"
+          ></div>
         </div>
       </div>
     </div>
@@ -15,29 +23,120 @@
 </template>
 
 <script>
-  import { storeHomeMixin } from '../../utils/mixin'
-  import { flapCardList } from '../../utils/store'
+    import { storeHomeMixin } from '../../utils/mixin'
+    import { flapCardList } from '../../utils/store'
 
-  export default {
-    mixins: [storeHomeMixin],
-    data () {
-      return {
-        flapCardList
-      }
-    },
-    methods: {
-      semiCircleStyle (item, position) {
-        return {
-          backgroundColor: `rgb(${item.r},${item.g},${item.b})`,
-          backgroundSize: item.backgroundSize,
-          backgroundImage: position === 'left' ? item.imgLeft : item.imgRight
+    export default {
+        mixins: [storeHomeMixin],
+        data () {
+            return {
+                flapCardList,
+                front: 0,
+                back: 1
+            }
+        },
+        watch: {
+            flapCardVisible (v) {
+                if (v) {
+                    this.startFlapCardAnimation()
+                }
+            }
+        },
+        methods: {
+            semiCircleStyle (item, position) {
+                return {
+                    backgroundColor: `rgb(${item.r},${item.g},${item.b})`,
+                    backgroundSize: item.backgroundSize,
+                    backgroundImage: position === 'left' ? item.imgLeft : item.imgRight
+                }
+            },
+            close () {
+                this.stopAnimation()
+                this.setFlapCardVisible(false)
+            },
+            rotate (index, type) {
+                const item = this.flapCardList[index]
+                let dom
+                if (type === 'front') {
+                    dom = this.$refs.right[index]
+                } else {
+                    dom = this.$refs.left[index]
+                }
+                dom.style.transform = `rotateY(${item.rotateDegree}deg)`
+                dom.style.backgroundColor = `rgb(${item.r},${item._g},${item.b})`
+            },
+            flapCardRotate () {
+                const frontCardItem = this.flapCardList[this.front]
+                const backCardItem = this.flapCardList[this.back]
+                frontCardItem.rotateDegree += 10
+                frontCardItem._g -= 5
+                backCardItem.rotateDegree -= 10
+                if (backCardItem.rotateDegree < 90) {
+                    backCardItem._g += 5
+                }
+                if (frontCardItem.rotateDegree === 90 && backCardItem.rotateDegree === 90) {
+                    backCardItem.zIndex += 2
+                }
+                this.rotate(this.front, 'front')
+                this.rotate(this.back, 'back')
+                if (frontCardItem.rotateDegree === 180 && backCardItem.rotateDegree === 0) {
+                    this.next()
+                }
+            },
+            next () {
+                const frontCardItem = this.flapCardList[this.front]
+                const backCardItem = this.flapCardList[this.back]
+                frontCardItem.rotateDegree = 0
+                backCardItem.rotateDegree = 0
+                frontCardItem._g = frontCardItem.g
+                backCardItem._g = backCardItem.g
+                this.rotate(this.front, 'front')
+                this.rotate(this.back, 'back')
+                this.front++
+                this.back++
+                const len = this.flapCardList.length
+                if (this.front >= len) {
+                    this.front = 0
+                }
+                if (this.back >= len) {
+                    this.back = 1
+                }
+                this.flapCardList.forEach((item, index) => {
+                    item.zIndex = 100 - ((index - this.front + len) % len)
+                })
+                this.prepare()
+            },
+            prepare () {
+                const backCardItem = this.flapCardList[this.back]
+                backCardItem.rotateDegree = 180
+                backCardItem._g = backCardItem.g - 45
+                this.rotate(this.back, 'back')
+            },
+            startFlapCardAnimation () {
+                this.prepare()
+                this.task = setInterval(() => {
+                    this.flapCardRotate()
+                }, 20)
+            },
+            stopAnimation () {
+                this.reset()
+                if (this.task) {
+                    clearInterval(this.task)
+                }
+            },
+            reset () {
+                this.front = 0
+                this.back = 1
+                this.flapCardList.forEach((item, index) => {
+                    item.zIndex = 100 - index
+                    item._g = item.g
+                    item.rotateDegree = 0
+                    this.rotate(index, 'front')
+                    this.rotate(index, 'back')
+                })
+            }
         }
-      },
-      close () {
-        this.setFlapCardVisible(false)
-      }
     }
-  }
 </script>
 
 <style lang="scss" scoped>
@@ -69,14 +168,17 @@
             height: 100%;
             width: 50%;
             background-repeat: no-repeat;
+            backface-visibility: hidden;
           }
           .flap-card-semi-circle-left{
             border-radius: px2rem(24) 0 0 px2rem(24);
+            transform-origin: right;
             background-position: center right;
 
           }
           .flap-card-semi-circle-right{
             border-radius: 0 px2rem(24) px2rem(24) 0;
+            transform-origin: left;
             background-position: center left;
           }
         }
